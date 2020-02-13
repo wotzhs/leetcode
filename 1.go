f084da5f3f3a08c2161a9b3dc81fb9313f60db76
Sean Wong <wotzhs@gmail.com>
2020-02-13 11:35:47 +0800

    master: remove unneeded loggings

--- 1.go
+++ 1.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func TwoSum(nums []int, target int) []int {
 	hm := map[int]*int{}
 	for k, v := range nums {
@@ -9,14 +7,8 @@ func TwoSum(nums []int, target int) []int {
 		hm[v] = &idx
 	}
 
-	for k, v := range hm {
-		fmt.Printf("k: %v v: %v\n", k, *v)
-	}
-
 	for k, v := range nums {
 		diff := target - v
-		fmt.Printf("diff: %v\n", diff)
-		fmt.Printf("hm[diff]: %v\n", *hm[diff])
 		if hm[diff] != nil {
 			if *hm[diff] == k {
 				continue
