5980f5dc38e4039021aa3daef8e30aa220d806d2
Sean Wong <wotzhs@gmail.com>
2020-05-20 11:19:39 +0800

    master: wip

---
+++ 21.js
@@ -0,0 +1,67 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    if (l1 === null) {
+        return l2
+    }
+    if (l2 === null) {
+        return l1
+    }
+    
+    while(l2 != null) {
+        if (l1.val >= l2.val) {
+            let tmp = l2
+            l2 = l2.next
+            tmp.next = l1
+            l1 = tmp
+            continue
+        }
+        
+        if (l1.val < l2.val){
+            if(l1.next === null) {
+                l1.next = l2
+                return l1
+            }
+            let count = 0
+            let tmp = l1.next
+            while(tmp.val <= l2.val) {
+                if (tmp.next === null) {
+                    break
+                }
+                console.log(tmp.val)
+                tmp = tmp.next
+                count++
+            }
+            let tmp3 = l1
+            while(count) {
+                tmp3 = tmp3.next
+                count--
+            }
+                console.log("updating")
+                let tmp2 = l2
+                l2 = l2.next
+                tmp2.next = tmp
+                tmp3.next = tmp2
+                console.log("updated")
+
+            console.log("out of while")
+            continue
+        }
+
+        
+        // l2 = l2.next
+    }
+    return l1
+};
+
+module.exports = mergeTwoLists
---
+++ 665.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Print(test())
+}
+
+func test() int {
+  A := []int{4, 2, 1}
+  scenarios := map[int][]int{}
+  var answer int
+  for i := range A {
+    tempArr := []int{}
+    tempArr = append(tempArr, A[0:i]...)
+    tempArr = append(tempArr, A[i+1:len(A)]...)
+    scenarios[i] = tempArr
+  }
+
+  fmt.Printf("scenarios %v\n", scenarios)
+
+  for _, v := range scenarios {
+    possible := true
+    for i := 0; i < len(v)-1; i++ {
+      if v[i] > v[i+1] {
+        possible = false
+      }
+    }
+    if possible {
+      answer += 1
+    }
+
+  }
+  return answer
+}
