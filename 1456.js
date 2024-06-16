00cb135baaa5c56b686c92591de9cb15d1a48cc4
Sean Wong <wotzhs@gmail.com>
2024-06-16 18:44:16 +0800

    1456

---
+++ 1456.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+
+const isVowel = (ch) => {
+	return ch == "a" || ch == "e" || ch == "i" || ch == "o" || ch == "u";
+};
+
+var maxVowels = function (s, k) {
+	let sum = 0;
+	for (var i = 0; i < k; i++) {
+		if (isVowel(s[i])) {
+			sum++;
+		}
+	}
+
+	let max = sum;
+
+	for (var i = k; i < s.length; i++) {
+		if (isVowel(s[i])) {
+			sum++;
+		}
+
+		if (isVowel(s[i - k])) {
+			sum--;
+		}
+
+		if (sum > max) {
+			max = sum;
+		}
+	}
+
+	return max;
+};
+
+console.log(maxVowels("abciiidef", 3));
+console.log(maxVowels("aeiou", 2));
+console.log(maxVowels("leetcode", 3));
