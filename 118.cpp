910fc1022b810c6e1edb9a71d75d5d35241141d3
Sean Wong <wotzhs@gmail.com>
2024-07-15 11:26:14 +0800

    118

---
+++ 118.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans{
+            {1},
+        };
+
+        for (int i=1; i<numRows;i++) {
+            vector<int> row;
+            row.emplace_back(1);
+            for (int j=1; j<ans[i-1].size();j++) {
+                row.emplace_back(ans[i-1][j-1]+ans[i-1][j]);
+            }
+
+            row.emplace_back(1);
+            ans.emplace_back(row);
+        }
+        return ans;
+    }
+};
